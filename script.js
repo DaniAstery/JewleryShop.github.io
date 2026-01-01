1f4a3580841ad0ba156bcd97b3d6afda35556e9f
Danieldoti <danizchrist@gmail.com>
2026-01-01 16:48:17 +0200

    cleaned script.js

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // PRODUCTS
   // ==========================
   const products = [
-    { id: 1, name: "Classic Dress", price: 49.99, image: "images/placeholder2.png", video: `${BACKEND_URL}/videos/sample.mp4` },
-    { id: 2, name: "Stylish Heels", price: 59.99, image: "images/headphone2.png", video: `${BACKEND_URL}/videos/sample.mp4` },
+    { id: 1, name: "Classic Dress", price: 49.99, image: "images/placeholder2.png", video: `${BACKEND_URL}/videos/OP-0002-4-7.00 Grams.mp4` },
+    { id: 2, name: "Stylish Heels", price: 59.99, image: "images/headphone2.png", video: `${BACKEND_URL}/videos/OP-0002-4-7.00 Grams.mp4` },
   ];
 
   // ==========================
