b73b3c02bad1671ba4ed05ea2babc72ebfce6358
Danieldoti <danizchrist@gmail.com>
2026-01-10 22:37:06 +0200

    separtated initaliztions for users and admins

--- script.js
+++ script.js
@@ -231,11 +231,11 @@ document.addEventListener("DOMContentLoaded", () => {
   
 // for shop and cart logic
       // Only run cart logic if cart exists
-      if (document.querySelector("#cart")) {
+      if (document.querySelector("#cart-btn")) {
         renderProducts();
         updateCart();
       }
-      
+
 //for admin dashboard logic
     // Only run admin logic if admin table exists
       if (document.querySelector("#ordersTable")) {
